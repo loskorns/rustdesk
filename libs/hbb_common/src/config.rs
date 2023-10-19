9821ae88524244b76f6dd57d1a14219f71d0729a
loskorns <38986883+loskorns@users.noreply.github.com>
2023-10-19 08:45:34 -0300

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,11 +90,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["45.7.252.188"];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "I9THtIlxCDcoOHilAaCt1SZf1xRMkPu7ignoS5pCgfk=",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
